59019bb60f9251b6be6f938b61e7b72de874b7f2
MuliroMatt <murilo.mattiuzzi@gmail.com>
2023-11-22 00:13:37 -0300

    melhorando o css

--- ecommerceti26/cabecalho2.php
+++ ecommerceti26/cabecalho2.php
@@ -11,10 +11,6 @@ if (isset($_SESSION['nomeusuario'])) {
 
 <link rel="stylesheet" href="./css/style2.css">
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,800&display=swap" rel="stylesheet">
-
     <header>
         <div class="header-container">
             <div class="content-left">
